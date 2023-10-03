9d815f3af24c6a9e1869e3f1edc0ed73ab65cc5f
Mani <mani.ebra@gmail.com>
2023-10-03 16:33:49 +0330

    changed port

--- main.js
+++ main.js
@@ -7,6 +7,6 @@ app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "build", "index.html"));
 });
 // if not in production use the port 5000
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4176;
 console.log("server started on port:", PORT);
 app.listen(PORT);
