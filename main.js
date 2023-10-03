11fa6c53c87e0102a37e530feb6286340452526e
Mani <mani.ebra@gmail.com>
2023-10-03 16:29:00 +0330

    git fixed expressjs functionality

---
+++ main.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+app.use(express.static("./build"));
+
+const path = require("path");
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "build", "index.html"));
+});
+// if not in production use the port 5000
+const PORT = process.env.PORT || 5000;
+console.log("server started on port:", PORT);
+app.listen(PORT);
